8984b14558a96e2f404e3dac028fe6e2b36bc720
Zoe Gumbs <yellow@pulsarlap-03.pulsar.local>
2015-09-14 13:05:36 +0100

    tracking for 'how we work pop up', pathways selections, healthcare revealer

--- Pulsar/code/js/app.slide.hcp_first.js
+++ Pulsar/code/js/app.slide.hcp_first.js
@@ -1,1 +1,30 @@
-document.addEventListener("presentationInit",function(){var n=app.slide.hcp_first={elements:{Btn:"img"},onEnter:function(t){$("#menu").hide(),app.addEvent("tap",n.showNext,n.element.Btn),$("img.btn").on("tap",function(){console.log("Hellooo"),$(this).next().show()})},onExit:function(n){$("#menu .hasChildren").next().slideUp()},showNext:function(n){console.log(this)}}});
+document.addEventListener("presentationInit",function(){
+	var n=app.slide.hcp_first={
+		elements:{Btn:"img"},
+		onEnter:function(t){
+			$("#menu").hide(),
+			app.addEvent("tap",n.showNext, document)				
+		},
+		onExit:function(n){
+			$("#menu .hasChildren").next().slideUp();
+
+			//submit position in items when leave slide
+			var active = $(".btn.active").last().attr('data-position');
+			
+			ag.submit.data({
+	          	unique: true,
+	          	category: 'Insight',
+	          	label: "Working with healthcare professionals",
+	          	value: active,
+	          	valueType: "percentage",
+	          	path: app.getPath()
+	        });
+		},
+		showNext:function(n){
+			//collect number of elements with the class active
+			var pos = $(".btn.active").length;
+			//add class active from next element
+			$(".btn").eq(pos).addClass('active');
+		}
+	}
+});
--- Pulsar/code/js/global_funcs.js
+++ Pulsar/code/js/global_funcs.js
@@ -1,1 +1,18 @@
-document.addEventListener("presentationInit",function(){$("#menu ul ul li, .local li").on("tap",function(){app.goTo("pulsar",$(this).data("slideshow"),$(this).data("content"))}),$("#goHome").on("tap",function(){app.goTo("pulsar","home","home_first")}),$("#toggleMenu").on("tap",function(){$("#menu").toggle()}),$("#menu li").eq(0).on("tap",function(){$(this).next().slideDown()}),$("#menu li").next().next().on("tap",function(){$(this).next().slideDown()})});
+document.addEventListener("presentationInit",function(){
+	$("#menu ul ul li, .local li").on("tap",function(){
+		app.goTo("pulsar",$(this).data("slideshow"),
+			$(this).data("content"))}
+		),
+	$("#goHome").on("tap",function(){
+		app.goTo("pulsar","home","home_first")
+	}),
+	$("#toggleMenu").on("tap",function(){
+		$("#menu").toggle()
+	}),
+	$("#menu li").eq(0).on("tap",function(){
+		$(this).next().slideDown()
+	}),
+	$("#menu li").next().next().on("tap",function(){
+		$(this).next().slideDown()
+	})
+});
