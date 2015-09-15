4ee54370a955ffb1e82e0a8a1eb5b4ad4d7c51df
Garry Bain <garry@pulsarhealthcare.com>
2015-09-15 11:43:26 +0100

    Added analytics for recording button clicks on homepage

--- Pulsar/code/build/js/app.slide.home_first.js
+++ Pulsar/code/build/js/app.slide.home_first.js
@@ -1,6 +1,7 @@
 document.addEventListener('presentationInit', function() {
 
-    var slide = app.slide.home_first = {
+    var buttonUsage = ["Button not clicked","Button not clicked","Button not clicked"],
+    slide = app.slide.home_first = {
     elements: {
       aboutBtn: "#aboutus",
       philBtn: "#philosophy",
@@ -16,19 +17,35 @@ document.addEventListener('presentationInit', function() {
 
     onExit: function(ele) {
     $('#menu .hasChildren').next().slideUp();
+
+        // submit to agnitio tracking
+            ag.submit.event({
+                unique: true,
+                category: 'Video Stats',
+                label: "Video duration",
+                value: buttonUsage,
+                path: app.getPath()
+            });
+
+
     },
 
 
     takemetoAbout: function(e) {
+       buttonUsage[0] = "About button used";
         app.goTo('pulsar', 'about', 'about_first')
     },      
 
     takemetoPhil: function(e) {
+        buttonUsage[1] = "Philosophy button used";
         app.goTo('pulsar', 'philosophy', 'philosophy_first')
+
     },   
 
     takemetoIns: function(e) {
+        buttonUsage[2] = "Insight button used";
         app.goTo('pulsar', 'insight', 'insight_first')
+
     }    
 
   };
