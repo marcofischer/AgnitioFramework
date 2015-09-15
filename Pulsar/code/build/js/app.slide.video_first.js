47b38e8da23231a075be0a70b39ae8190530fad3
Garry Bain <garry@pulsarhealthcare.com>
2015-09-15 09:39:18 +0100

    Video code is working to record duration

--- Pulsar/code/build/js/app.slide.video_first.js
+++ Pulsar/code/build/js/app.slide.video_first.js
@@ -1,32 +1,54 @@
 document.addEventListener('presentationInit', function() {
 
-    var slide = app.slide.video_first = {
+    var currentTime1,
+    slide = app.slide.video_first = {
     elements: {
-        test : "#me",
+        myvideo : "#thisvideo",
 
     },
 
     onEnter: function(ele) {
          $('#menu').hide();   
-         app.addEvent('tap', slide.startVideo, slide.element.test);   
+         app.addEvent('tap', slide.startVideo, slide.element.myvideo);   
 
     },
 
     onExit: function(ele) {
-    $('#menu .hasChildren').next().slideUp();
-       
-        $('#me').pause; 
-       var currentTime1 = $('#me').currentTime;
-        console.log(currentTime1);
+
+     $('#menu .hasChildren').next().slideUp();
+      
+       $("#thisvideo")[0].pause(); 
+       // get current time
+       currentTime1 =  $('#thisvideo')[0].currentTime;
+       //console.log(currentTime1);
+
+        // submit to agnitio tracking
+            ag.submit.event({
+                unique: true,
+                category: 'Video Stats',
+                label: "Video duration",
+                value: currentTime1,
+                path: app.getPath()
+            });
+
 
     },
 
 
     startVideo: function(e) {
-       $('#me').pause(); 
-        // get current time
-     //   var currentTime1 = $('#me').currentTime;
-     //   console.log(currentTime1);
+        
+    // Allow the video to be clicked and paused etc as you'd expect it to
+
+        if ($("#thisvideo")[0].paused) {
+       
+             $('#thisvideo')[0].play(); // plays the video 
+
+        } else {
+       
+             $("#thisvideo")[0].pause();
+        
+        }
+
 
 }
 
