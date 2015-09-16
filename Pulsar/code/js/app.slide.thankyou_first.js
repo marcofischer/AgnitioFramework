1b98ac20f339a86402263051316731b40109b257
Garry Bain <garry@pulsarhealthcare.com>
2015-09-16 15:57:22 +0100

    Added thank you page

---
+++ Pulsar/code/js/app.slide.thankyou_first.js
@@ -0,0 +1,1 @@
+document.addEventListener("presentationInit",function(){app.slide.thankyou_first={elements:{},onEnter:function(n){$("#menu").hide()},onExit:function(n){$("#menu .hasChildren").next().slideUp()}}});
